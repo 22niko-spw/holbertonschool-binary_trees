b019f57b54010c8cf956931e3f9eac2b086b0c53
splint314 <krigal323@gmail.com>
2026-04-09 08:13:55 +0000

    z

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,7 +4,7 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 
-    if(!node)
-        return(0);
-    return (node->left == NULL && node ->right == NULL);
+	if (!node)
+		return (0);
+	return (node->left == NULL && node->right == NULL);
 }
